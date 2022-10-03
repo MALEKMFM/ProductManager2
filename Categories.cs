4fcfa98408e920a4bdced913750e8b1c52d7923e
Malek <malek@ec.se>
2022-10-03 15:39:32 +0200

    Add register Category

---
+++ Categories.cs
@@ -0,0 +1,29 @@
+﻿namespace ProductManager
+{
+
+     class Categories
+    {
+        public Categories()
+        {
+            Name = name;
+           
+        }
+
+        public string Name {
+            get
+            {
+                return name;
+            }
+            
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                    Console.WriteLine("Namnet kan inte vara tomt");
+                
+                name = value;
+            }
+        }
+
+        public string name;
+    }
+}
