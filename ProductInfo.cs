6f96d31933cd378e5d9bc9071284123d46accd94
Malek <malek@ec.se>
2022-10-03 15:38:09 +0200

    Add register product

---
+++ ProductInfo.cs
@@ -0,0 +1,11 @@
+﻿namespace ProductManager
+{
+
+    public class ProductInfo
+    {
+        public string name { get; set; }
+        public string articleNumber { get; set; }
+        public string? description { get; set; }
+        public int price { get; set; }
+    }
+}
